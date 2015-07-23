e37af19fc6a4de789affeafaf9e7ee20a550b0fb
anit <anit.rai011@gmail.com>
2015-07-24 01:03:01 +0530

    added new files and folder and restructing the whole flow

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,9 @@
 // Declare app level module which depends on views, and components
 var hexagon = angular.module('hexagon',[
   'ngRoute',
-  'ui.router'
+  'ui.router',
+  'ui.bootstrap',
+  'picardy.fontawesome'
 ])
 
 
@@ -22,12 +24,33 @@ var hexagon = angular.module('hexagon',[
         controller: 'hexagonCtrl'
       }
     );
-
 }])
+
+.config([
+  '$urlRouterProvider',
+  function ($urlRouterProvider) {
+    $urlRouterProvider.otherwise('hexagon');
+  }
+])
 
 .controller('hexagonCtrl', [
   '$scope',
   function ($scope) {
-    $scope.header = ['Home', 'About', 'Works', 'Services', 'Blog', 'Contact']
+    $scope.header = ['Home', 'About', 'Works', 'Services', 'Blog', 'Contact'];
+
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+        image: '//placekitten.com/' + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      });
+    };
+    for (var i = 0; i < 4; i++) {
+      $scope.addSlide();
+    }
   }
 ]);
